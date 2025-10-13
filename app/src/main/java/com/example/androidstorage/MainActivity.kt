7033bdd5b7185c7cf4c62010334c3460035b40d0
Abhishekverma2006 <j472812@gmail.com>
2025-10-13 23:52:48 +0530

    13-10-2025 23.52

--- app/src/main/java/com/example/androidstorage/MainActivity.kt
+++ app/src/main/java/com/example/androidstorage/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -33,7 +34,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun StorageApp() {
     val tabs = listOf("Internal", "External/Scoped", "Shared")
-    var selectedTab by remember { mutableStateOf(0) }
+    var selectedTab by remember { mutableIntStateOf(0) }
 
     Scaffold(
         topBar = {
